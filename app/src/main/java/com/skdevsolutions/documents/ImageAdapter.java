a645c9e933110eada7ade62233005ae04fc23c33
Sean <seankovacs9@gmail.com>
2014-08-20 16:40:23 -0400

    .

---
+++ app/src/main/java/com/skdevsolutions/documents/ImageAdapter.java
@@ -0,0 +1,64 @@
+package com.skdevsolutions.documents;
+
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.File;
+
+/**
+ * Created by Sean on 8/20/2014.
+ */
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File appDir = new File(storageDir,"DocPics");
+        File file[] = appDir.listFiles();
+
+        return file.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {  // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(150, 150));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(2, 2, 2, 2);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File appDir = new File(storageDir,"DocPics");
+        File file[] = appDir.listFiles();
+
+        Bitmap bitmap = BitmapFactory.decodeFile(file[position].getAbsoluteFile().toString());
+
+        imageView.setImageBitmap(bitmap);
+        return imageView;
+    }
+
+}
---
+++ app/src/main/java/com/skdevsolutions/documents/MainActivity.java
@@ -0,0 +1,190 @@
+package com.skdevsolutions.documents;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.Toast;
+import android.util.Log;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
+import com.soundcloud.android.crop.Crop;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class MainActivity extends Activity {
+    public static final int REQUEST_TAKE_PHOTO = 1;
+    public static final int REQUEST_CROP_PHOTO = Crop.REQUEST_CROP;
+    private String mCurrentPhotoPath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "" + i, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_camera) {
+            launchCamera();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_TAKE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    // Image captured and saved to fileUri specified in the Intent
+
+                    Uri outputUri = Uri.fromFile(new File(mCurrentPhotoPath));
+                    Uri source = Uri.fromFile(new File(mCurrentPhotoPath));
+                    new Crop(source).output(outputUri).asSquare().start(this);
+                }
+                break;
+            case REQUEST_CROP_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    // Image captured and saved to fileUri specified in the Intent
+                    GridView gridview = (GridView) findViewById(R.id.gridview);
+                    ((BaseAdapter)gridview.getAdapter()).notifyDataSetChanged();
+
+                    // Prompt for email
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+                    // set title
+                    alertDialogBuilder.setTitle("Email Document?");
+
+                    // set dialog message
+                    alertDialogBuilder
+                            .setMessage("Would you like to send this via email?")
+                            .setCancelable(false)
+                            .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,int id) {
+                                    // if this button is clicked, close
+                                    // current activity
+                                    Intent intent = new Intent(Intent.ACTION_SEND);
+                                    intent.setType("application/image");
+                                    intent.putExtra(Intent.EXTRA_SUBJECT, "Document");
+                                    intent.putExtra(Intent.EXTRA_TEXT, "Document captured via Documents for Android.");
+                                    intent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file:"+mCurrentPhotoPath));
+                                    startActivity(Intent.createChooser(intent, "Send Email"));
+                                }
+                            })
+                            .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,int id) {
+                                    // if this button is clicked, just close
+                                    // the dialog box and do nothing
+                                    dialog.cancel();
+                                }
+                            });
+
+                    // create alert dialog
+                    AlertDialog alertDialog = alertDialogBuilder.create();
+
+                    // show it
+                    alertDialog.show();
+                }
+                break;
+        }
+    }
+
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    public void launchCamera() {
+        if(checkCameraHardware(MainActivity.this)) {
+            // Kick off camera
+            dispatchTakePictureIntent();
+        }else {
+            Toast.makeText(MainActivity.this, "This device doesn't have a camera.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Toast.makeText(this, "Error creating file.", Toast.LENGTH_LONG).show();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File appDir = new File(storageDir,"DocPics");
+        appDir.mkdirs();
+
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                appDir      /* directory */
+        );
+
+        Log.d("Path",image.toString());
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+}
